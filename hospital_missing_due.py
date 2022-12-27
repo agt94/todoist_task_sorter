a98f9184986f4e8fb574c9692afba4741d6ab174
agt94 <75878807+agt94@users.noreply.github.com>
2022-12-27 23:47:13 +0100

    no calendar anymore

--- hospital_missing_due.py
+++ hospital_missing_due.py
@@ -59,20 +59,9 @@ class Todoist(object):
         self.hospital = self.get_hospital()
         print(self.hospital)
 
-        calendar_label_ids = [label.id for label in self.api.labels if label.name == 'calendar']
-        self.to_calendar_label = 'to_calendar'
-        self.calendar_label = 'calendar'
-        to_calendar_label_ids = [label.id for label in self.api.labels if label.name == 'to_calendar']
-        # in_calendar_label_ids = [label['id'] for label in self.api.state['labels'] if label['name'] == 'in_calendar']
-        calendar_project_id = [project.id for project in self.api.projects if project.name == 'calendar']
-        self.calendar_project_id = calendar_project_id[0]
-        self.calendar_label_id = calendar_label_ids[0]
-        self.to_calendar_label_id = to_calendar_label_ids[0]
-        #self.in_calendar_label_id = in_calendar_label_ids[0]
-        self.calendar = self.get_calendar()
-        print(self.calendar)
-        self.tasks_in_calendar =self.get_tasks_calendar_project()
         self.missing_due = self.get_no_duedate()
+        print("Missing due")
+        print(self.missing_due)
 # Funciones para asignar etiquetas de hospital a tareas con numeros de historia o palabras clave
     def get_hospital(self):
         hospital = []
@@ -90,179 +79,29 @@ class Todoist(object):
             new_labels.append(self.hospital_label)
             self.api.update_task(task_id = item.id, labels = new_labels, project_id = self.hospital_id )
 
-# Asignar la @calendar a tareas de alta prioridad para que mediante un applet ppuedan aparecer en calendario de Google y en widget.
-    def get_calendar(self):
-        calendar = []
-        for item in self.api.notes:
-            if (item.priority >2 or self.to_calendar_label_id in item.labels) and item.due is not None and self.calendar_label not in item.labels and  self.calendar_project_id  != item.project_id: #tareas prioritarias y que no tengan ya asignada esa etiqueta
-                calendar.append(item)
-
-        return calendar
-
-
-    ######################## por adaptar
-
-    def get_tasks_calendar_project(self):
-        tasks_in_calendar = []
-        for item in self.api.state['items']:
-            if self.calendar_project_id  == item['project_id'] and item['checked']== 0: #tareas en el proyecto calendar y no completadas
-                tasks_in_calendar.append(item)
-
-        return tasks_in_calendar
-
-    def update_calendar(self):
-
-        for item in self.calendar:
-            #print(item['due'])
-            #print(item['due']['date'])
-            #print(item['due']['datetime'])
-            #print(item['due']['is_recurring'])
-            #print(item['due']['string'])
-            #print(item['due']['lang'])
-            try:
-
-                ############### eliminar______________________________________________
-                new_labels = item['labels']
-                #new_labels.append(self.in_calendar_label_id)
-                task = self.api.add_item(
-                    content= item['content']+"_",
-                    #due_date={"string": "today"},
-                    date_string = item['due']['string'],
-                    date_lang = 'es',
-                    #due = {"string": "today"},
-                    priority= item['priority'],
-                    project_id = self.calendar_project_id,
-                    labels = new_labels
-                )
-                due_date_long = {'date': item['due']['date'], 'is_recurring': item['due']['is_recurring'] ,'lang': item['due']['lang'],
-                          'string':item['due']['string'], 'timezone': item['due']['timezone']
-
-                          }
-                #due_date = {"string": "today"}
-                #task.update(due = due_date_long)
-                self.api.commit()
-                #print(task)############### eliminar______________________________________________
-                new_labels = item['labels']
-                new_labels.append(self.calendar_label_id)
-                item.update(labels=new_labels)
-
-            except Exception as error:
-                print(error)
-        self.api.commit()
-
-    def delete_calendar(self): #closes tasks for which the parent task has been completed or changed name
-        calendar_labelled_content = []
-        calendar_labelled_notes = []
-        for item in self.api.state['items']:
-            if self.calendar_label_id in item['labels'] and  self.calendar_project_id != item['project_id']and item['checked']== 0:
-                calendar_labelled_content.append(item['content'])
-                calendar_labelled_notes.append(item)
-                        
-        #print(calendar_labelled_content)
-        for item in self.tasks_in_calendar: #toma las tareas del proyecto calendar
-           # print(item['content'])
-            #print(len(self.tasks_in_calendar))
-            #print(calendar_labelled_content)
-            #print(item['content'][:-1] not in calendar_labelled_content)
-
-
-            if item['content'][:-1] not in calendar_labelled_content:
-               # print(item['content'])
-                item.update(checked=1)
-                #item.update(is_deleted=1)
-                self.api.commit()
-
-
-            else: #corregir cuando se reprograma una tarea a otra fecha para que le siga en el proyecto calendar
-
-                original_twin = calendar_labelled_notes[calendar_labelled_content.index(item['content'][:-1])]
-                if original_twin['due'] != item['due']:
-                    #print(item['content'])
-                    #print(item['due'])
-                    #print(original_twin['content'])
-                    #print(original_twin['due'])
-                    item.update_date_complete(due=original_twin['due'])
-
-        self.api.commit()
-
-    def create_calendar(self):  # creates calendar tasks if parent task is updated
-        calendar_labelled_content = []
-        calendar_labelled_notes = []
-        tasks_in_calendar_content = []
-        self.tasks_in_calendar = self.get_tasks_calendar_project()
-
-        for item in self.api.state['items']:
-            if self.calendar_label_id in item['labels'] and self.calendar_project_id != item['project_id']and item['checked']== 0 and item['due'] is not None: #corrige el error al quitarle la fecha a una tarea parent pero nos dejamos la etiqueta calendar
-                calendar_labelled_content.append(item['content'])
-                calendar_labelled_notes.append(item)
-
-        for item in self.tasks_in_calendar:
-            tasks_in_calendar_content.append(item['content'])
-
-        tasks_needing_calendar_copy = []
-        for item in calendar_labelled_notes:
-
-            if item['content']+"_" not in tasks_in_calendar_content:
-                tasks_needing_calendar_copy.append(item)
-
-        for item in tasks_needing_calendar_copy:
-            new_labels = item['labels']
-            try:
-                task = self.api.add_item(
-                    content=item['content'] + "_",
-
-                    date_string=item['due']['string'],
-                    date_lang='es',
-
-                    priority=item['priority'],
-                    project_id=self.calendar_project_id,
-                    labels=new_labels
-                )
-            except Exception as error:
-                print(error)
-                print(item)
-            #new_labels = item['labels']
-            #new_labels.append(self.calendar_label_id)
-            #task.update(labels=new_labels)
-            self.api.commit()
-
-
-
-
-
-
     # Dar fecha a aquellas tareas que se hayan añadido a la bandeja de entrada pero que no sean subtareas.
     def get_no_duedate(self):
         missing_due = []
 
-        for item in self.api.state['items']:
-            if self.inbox_id == item['project_id']:
+        for item in self.api.notes:
+            if self.inbox_id == item.project_id:
 
-                if  item['parent_id'] is None and item['due'] is None: #sin fecha y que no sean subtasks
+                if  item.parent_id is None and item.due is None: #sin fecha y que no sean subtasks
                     missing_due.append(item)
 
         return missing_due
 
     def update_missing_due(self):
         for item in self.missing_due:
+            due_date = {"string": "today"}
+            self.api.update_task(task_id = item.id, due = due_date)
 
-
-                due_date = {"string": "today"}
-                item.update(due=due_date)
-
-
-        self.api.commit()
 
 def main():
     todo = Todoist()
-    todo.update_missing_due()
     todo.update_hospital()
+    todo.update_missing_due()
 
-
-    todo.update_calendar()
-
-    todo.create_calendar()
-    todo.delete_calendar()
     print("run succesfully!")
 
 if __name__ == '__main__':
