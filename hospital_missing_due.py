5b5f7ca6540ed2953a8973a1fde5864e543d31f6
agt94 <75878807+agt94@users.noreply.github.com>
2022-12-28 17:35:51 +0100

    all good

--- hospital_missing_due.py
+++ hospital_missing_due.py
@@ -2,7 +2,12 @@ import os
 import re
 import logging
 from datetime import datetime
-from dateutil import tz
+#from dateutil import tz
+
+from todoist_api_python.api import TodoistAPI
+from uuid import uuid4
+import requests
+
 #from todoist.api import TodoistAPI
 #from todoist.managers.notes import NotesManager
 #from todoist.managers.items import ItemsManager
@@ -13,38 +18,34 @@ TODOIST_DATE_FORMAT = "%Y-%m-%d"
 
 red_dot = '\U0001f534'
 
+
 def get_token():
     token = os.getenv('TODOIST_APIKEY')
     if not token:
         raise Exception('Please set the API token in environment variable.')
     return token
 
-from todoist_api_python.api import TodoistAPI
+TOKEN = get_token()
 
 #https://github.com/Doist/todoist-api-python/issues/8  move tasks to different project
 
 class Todoist_program(object):
     def __init__(self):
-        self.api = TodoistAPI(get_token())
+        self.api = TodoistAPI(TOKEN)
 
-        #print(dir(self.api))
-        #self.api.sync()
         self.api.notes = self.api.get_tasks()
         self.api.projects = self.api.get_projects()
         self.api.labels = self.api.get_labels()
-        print(self.api.labels)
-        print("Print inbox:")
-        print((self.api.projects[0].name))
-        print(type(self.api.projects ))
-
+        self.api.sections = self.api.get_sections()
+        self.testing_id = [project.id for project in self.api.projects if project.name == 'testing'][0]
+        self.section_heaven_id = [section.id for section in self.api.sections if section.name == 'Heaven'][0]
         hospital_id = [project.id for project in self.api.projects if project.name == 'hospital']
 
         inbox_id = [project.id for project in self.api.projects if project.name == 'Inbox']
-
+        calendar_id = [project.id for project in self.api.projects if project.name == 'calendar']
         self.inbox_id = inbox_id[0]
         self.hospital_id = hospital_id[0]
-        print(self.inbox_id)
-        print(self.hospital_id)
+        self.calendar_project_id = calendar_id[0]
 
         self.hospital_label = 'hospital'
         hospital_label_ids = [label.id for label in self.api.labels if label.name == 'hospital']
@@ -54,77 +55,42 @@ class Todoist_program(object):
 
         assert (len(hospital_label_ids) == 1)
         self.hospital_label_id = hospital_label_ids[0]
-        print("Print hospital label_id ")
-        print(self.hospital_label_id)
+
 
         self.hospital = self.get_hospital()
-        print(self.hospital)
 
         self.missing_due = self.get_no_duedate()
         print("Missing due")
         print(self.missing_due)
 
-    def move_task(task, project_id: str = None, section_id: str = None, \
-                  parent_id: str = None, order: int = None):
-        """'Moves' a task to a different project/ section or parent by creating a \
-            identical new one and deleting the old one.
+        print("Test notes:")
+        self.test_notes = self.get_test_notes()
+        print(self.test_notes)
+        ##### Moving tasks:
+        section_id= self.section_heaven_id
 
-        Args:
-            task (TodoistAPI task object): task-object of the task to move.
-            project_id (str, optional): Where the task should be moved to. \
-                Defaults to None.
-            section_id (str, optional): Where the task should be moved to. \
-                Defaults to None.
-            parent_id (str, optional): Where the task should be moved to. \
-                Defaults to None.
-            order (int, optional): Where the task should be moved to. \
-                Defaults to None.
-
-        Returns:
-            task (TodoistAPI task object): If successful. Else returns None.
-        """
-        api = TodoistAPI(get_token())
-        try:
-            comments = api.get_comments(task_id=task.id)
-        except Exception as error:
-            print(error)
-            return False
-        else:
-            try:
-                new_task = api.add_task(
-                    content=task.content,
-                    description=task.description,
-                    labels=task.labels,
-                    priority=task.priority,
-                    due=task.due,
-                    assignee_id=task.assignee_id,
-                    project_id=project_id,
-                    section_id=section_id,
-                    parent_id=parent_id,
-                    order=order
-                )
-            except Exception as error:
-                print(error)
-                return False
-            else:
-                for comment in comments:
-                    try:
-                        created_comment = api.add_comment(
-                            content=comment.content,
-                            task_id=task.id,
-                            attachment=comment.attachment
-                        )
-                    except Exception as error:
-                        print(error)
-                        return False
-                    else:
-                        try:
-                            api.delete_task(task_id=task.id)
-                        except Exception as error:
-                            print(error)
-                            return False
-                        else:
-                            return new_task
+    def move_task(self, task_id: str, project_id: str) -> bool:
+        body = {
+            "commands": [
+                {
+                    "type": "item_move",
+                    "args": {"id": task_id, "project_id": project_id},
+                    "uuid": uuid4().hex,
+                },
+            ],
+        }
+        response = requests.post(
+            "https://api.todoist.com/sync/v9/sync",
+            headers={"Authorization": f"Bearer {TOKEN}"},
+            json=body,
+        )
+        return response.ok
+    def get_test_notes(self):
+        test = []
+        for item in self.api.notes:
+            if item.project_id == self.testing_id: #
+                test.append(item)
+        return test
 
 # Funciones para asignar etiquetas de hospital a tareas con numeros de historia o palabras clave
 
@@ -143,6 +109,7 @@ class Todoist_program(object):
             new_labels = item.labels
             new_labels.append(self.hospital_label)
             self.api.update_task(task_id = item.id, labels = new_labels )
+            self.move_task(task_id=item.id, project_id=self.hospital_id)
             #project_id = self.hospital_id  faltaría mover las tareas al proyecto hospital
 
     # Dar fecha a aquellas tareas que se hayan añadido a la bandeja de entrada pero que no sean subtareas.
@@ -152,22 +119,29 @@ class Todoist_program(object):
         for item in self.api.notes:
             if self.inbox_id == item.project_id:
 
-                if  item.parent_id is None and item.due is None: #sin fecha y que no sean subtasks
+                if item.parent_id is None and item.due is None: #sin fecha y que no sean subtasks en el Inbox
                     missing_due.append(item)
 
         return missing_due
 
     def update_missing_due(self):
         for item in self.missing_due:
-            due_date = {"string": "today"}
-            self.api.update_task(task_id = item.id, due = due_date)
+            due_date = "today"
+            self.api.update_task(task_id = item.id, due_string = due_date)
 
+    def send_to_calendar(self):
+        for item in self.api.notes:
+            if ((item.priority >= 3 ) or (self.calendar_label in item.labels )) and self.calendar_project_id != item.project_id and item.due is not None :
+                new_labels = item.labels
+                new_labels.append(self.calendar_label)
+                self.api.update_task(task_id=item.id, labels=new_labels)
+                self.move_task(task_id=item.id, project_id=self.calendar_project_id)
 
 def main():
     todo = Todoist_program()
     todo.update_hospital()
     todo.update_missing_due()
-
+    todo.send_to_calendar()
     print("run succesfully!")
 
 if __name__ == '__main__':
